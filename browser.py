1ce0654d96873e3d5e003ba7a5d7171cf0603961
28mirceas <mir.sava@gmail.com>
2026-05-22 07:53:21 +0300

    Update browser.py

--- browser.py
+++ browser.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 
+
 class Browser:
 
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.implicitly_wait(5)
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(5)
+
 
     def close(self):
         self.driver.quit()
