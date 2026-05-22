69b4cc6de3d2c7c0114420c30f3ebfdc584be233
28mirceas <mir.sava@gmail.com>
2026-05-22 07:44:21 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,31 @@
-from browser import Browser
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
 
-class BasePage(Browser):
+
+class BasePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+
     def find(self, locator):
-        return self.driver.find_element(*locator)
+        return self.wait.until(EC.visibility_of_element_located(locator))
+
 
     def type(self, locator, text):
-        self.find(locator).send_keys(text)
+        element = self.find(locator)
+        element.clear()
+        element.send_keys(text)
+
 
     def click(self, locator):
-        self.find(locator).click()
+        element = self.wait.until(EC.element_to_be_clickable(locator))
+        element.click()
+
+
+    def select_item_by_text(self, locator, text):
+        dropdown_element = self.wait.until(EC.element_to_be_clickable(locator))
+        dropdown = Select(dropdown_element)
+        dropdown.select_by_visible_text(text)
