27e2acb1fc42be6d8c0fd86f19641d0db9865863
28mirceas <mir.sava@gmail.com>
2026-05-26 08:27:29 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -14,6 +14,10 @@ class BasePage:
         return self.wait.until(EC.visibility_of_element_located(locator))
 
 
+    def find_multiple(self, locator):
+        return self.wait.until(EC.visibility_of_all_elements_located(locator))
+    
+
     def type(self, locator, text):
         element = self.find(locator)
         element.clear()
@@ -29,3 +33,11 @@ class BasePage:
         dropdown_element = self.wait.until(EC.element_to_be_clickable(locator))
         dropdown = Select(dropdown_element)
         dropdown.select_by_visible_text(text)
+
+
+    def verify_current_url(self, expected_url):
+        assert self.driver.current_url == expected_url, (
+            f"Expected URL: {expected_url}, "
+            f"but got: {self.driver.current_url}"
+        )
+
